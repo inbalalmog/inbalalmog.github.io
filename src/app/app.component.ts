88b9de8f0c0fd4593492849e409f4346f8f48857
Inbal almog <almog.inbal@gmail.com>
2017-11-11 00:59:44 +0200

    get dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+var request = require('request');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  version: string;
+  package: string;
+  title = 'Snyk';
+  onclick() {
+    console.log('click');
+    this.fetchDependencies(this.package, this.version);
+  }
+  fetchDependencies(packageName, version) {
+    console.log('fetchDependencies');
+    const url = 'https://registry.npmjs.org/' + packageName + '/' + version;
+    let myRequest = new Request(url);
+    fetch(myRequest)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+        console.log(response['dependencies']);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {InputTextModule} from 'primeng/primeng';
+
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/components/button/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    FormsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
