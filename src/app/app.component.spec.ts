12c1611fabce8d6c2cff627b22d47a00a97051c8
Inbal almog <almog.inbal@gmail.com>
2017-11-12 21:53:45 +0200

    add test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,30 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {InputTextModule, TreeModule, TreeNode, ButtonModule} from 'primeng/primeng';
+import { FormsModule } from '@angular/forms';
+import {} from 'jasmine';
+import { TreeService } from './services/tree.service';
+import { BrowserModule } from '@angular/platform-browser';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        InputTextModule,
+        FormsModule,
+        ButtonModule,
+        TreeModule
+      ],
       declarations: [
         AppComponent
       ],
+      providers: [TreeService]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
--- src/app/services/tree.service.spec.ts
+++ src/app/services/tree.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
-
 import { TreeService } from './tree.service';
+import { TreeNode } from 'primeng/primeng';
+import {} from 'jasmine';
 
 describe('TreeService', () => {
   beforeEach(() => {
@@ -12,4 +13,17 @@ describe('TreeService', () => {
   it('should be created', inject([TreeService], (service: TreeService) => {
     expect(service).toBeTruthy();
   }));
+
+
+  it('express dependencies', inject([TreeService], (service: TreeService) => {
+    const node: TreeNode = {
+      label: 'express',
+      data: 'latest',
+      children: []
+    };
+    const promise = service.fetchDependencies(node);
+    promise.then((testedResult) =>
+      expect(Object.keys(testedResult).length).toBe(30)
+    );
+ }));
 });
